a63f8d187d4fbe8e46dda000dce8328c09dfc445
robbisg <robbenson18@gmail.com>
2017-08-21 16:32:17 +0200

    feat: Added routing and BookItem component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Route , Link } from 'react-router-dom'
 import BookList from "./BookList"
+import SearchBook from "./SearchBook"
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -28,29 +30,15 @@ class BooksApp extends React.Component {
 
   render() {
     return (
+
+
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
+        <Route exact path="/search" render={ () =>
+          <SearchBook />
+        } />
 
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
+        <Route exact path="/" render={ () =>
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -66,10 +54,11 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to="/search">Add a book</Link>
             </div>
           </div>
-        )}
+        } />
+
       </div>
     )
   }
--- src/BookList.js
+++ src/BookList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import BookItem from "./BookItem"
 
 class BookList extends Component {
 
@@ -25,26 +26,7 @@ class BookList extends Component {
         <h2 className="bookshelf-title">{this.state.titleDict[this.props.shelf]}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {shelfBooks.map((book) => (
-              <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>
-                    <div className="book-shelf-changer">
-                      <select>
-                        <option value="none" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                  <div className="book-authors">{book.author}</div>
-                </div>
-              </li>
-            ))}
+            {shelfBooks.map((book) => (<BookItem book={book} />))}
           </ol>
         </div>
       </div>
